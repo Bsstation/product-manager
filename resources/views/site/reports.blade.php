5b8efbc76b7b3209ed5437dde373ffc0a577f517
Anderson Santos <andersonmatheus011@hotmail.com>
2025-02-09 14:08:14 -0300

    Cria página de relatórios

--- resources/views/site/reports.blade.php
+++ resources/views/site/reports.blade.php
@@ -3,4 +3,20 @@
 @section('title', 'Relatórios')
 
 @section('content')
+    <div class="row">
+        <h1>Relatórios</h1>
+        <div class="col s12 m6">
+            <h3>Clique no botão ao lado para gerar um relatório.</h3>
+        </div>
+        <!-- Modal Trigger -->
+        <div class="center-align col s12 m6">
+            <br><br>
+            <a class="waves-effect waves-light btn-large modal-trigger" href="#modal1">Adicionar</a>
+        </div>
+
+        <!-- Modal Structure -->
+        <div id="modal1" class="modal col">
+            @include('forms.new-movement')
+        </div>
+    </div>
 @endsection
