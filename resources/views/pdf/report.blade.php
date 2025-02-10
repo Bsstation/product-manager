63537c59a054fc9ff787ff7972ae25a2d6589777
Anderson Santos <andersonmatheus011@hotmail.com>
2025-02-09 23:09:02 -0300

    Adiciona tabela ao pdf do relatório

--- resources/views/pdf/report.blade.php
+++ resources/views/pdf/report.blade.php
@@ -1,1 +1,47 @@
-sdsadsada
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ $title }}</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Movimentações</h1>
+        @if($movements->count() > 0)
+            <div class="row">
+                <table class="striped">
+                    <thead>
+                        <tr>
+                            <th>Data</th>
+                            <th>Produtos</th>
+                            <th>Fluxo</th>
+                            <th>Empresa</th>
+                            <th>Realizado por</th>
+                            <th>Frete</th>
+                            <th>Total</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            @foreach ($movements as $movement)
+                                <td>{{$movement->getFormatedDate()}}</td>
+                                <td>{{$movement->getProductsFormatedNames()}}</td>
+                                <td>{{$movement->getFormatedFlux()}}</td>
+                                <td>{{$movement->company->name}}</td>
+                                <td>{{$movement->user->name}}</td>
+                                <td>{{$movement->getFormatedDeliveryValue()}}</td>
+                                <td>{{$movement->getTotalValue(true)}}</td>
+                            @endforeach
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        @else
+            <div class="col s12 m6">
+                <h3>Ainda não há movimentações, vamos adicionar uma?</h3>
+            </div>
+        @endif
+    </div>
+</body>
+</html>
